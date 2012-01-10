812a1f4be4da3ea6f3ad1994cca0a077695078c1
Jay Parekh <jay@JUbuntu.(none)>
2012-01-10 13:29:47 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '0f705644b1ff478942958c0a0e5bb92a611e41d1540ed62c1090bf189645b559735e6f1c01798b5db5ea244c84c03348ef3f564e9a8343aaa5e5deb4684cb2f9'
